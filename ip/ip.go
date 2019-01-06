ed8f521024c6e223ec37bd6ba4cc3184e8cad85c
dengxiaochuan <1554694323@qq.com>
2019-01-06 22:38:35 +0800

    delete util

---
+++ ip/ip.go
@@ -0,0 +1,41 @@
+package ip
+
+import (
+	"errors"
+	"log"
+	"net"
+)
+
+func InnerIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("no local ip")
+}
+
+func MustInnerIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	log.Fatal("have no ip")
+	return ""
+}
